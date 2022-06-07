0c4e883ccfa8a0fdf6750f163f74ed745bf437fb
devklucas <lucascardoso64@gmail.com>
2022-06-06 23:16:43 -0300

    feat: create header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from '../../assets/logoBlack.png'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width:100vw;
+    background-color:var(--Grey-1);
+    height:60px;
+    box-shadow: 0px 4px 20px -12px #000000;
+`
+const BoxContent = styled.div`
+    width:90%;
+    height:100%;
+    margin: 0 auto;
+    align-items: center;
+    display:flex;
+    justify-content:space-between;
+`
+const ButtonStyled = styled.button`
+    width:64px;
+    height:40px;
+    background-color:var(--Grey-2);
+    border-radius: 8px;
+`
+export const Header = () => {
+    return (
+        <Container>
+            <BoxContent>
+                <img src={logo} alt='logomarca'/>
+                <ButtonStyled>Inicio</ButtonStyled>
+            </BoxContent>        
+        </Container>
+
+    )
+}
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Header } from '../../components/header'
 
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
+        <Header/>
     )
 }
 export default Dashboard
